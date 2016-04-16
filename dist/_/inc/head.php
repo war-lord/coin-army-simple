27d5b5a3f2a31c828947fc4fec6133cc72a4b40a
war-lord <warlord1@protonmail.com>
2016-04-16 03:19:50 +0300

    Initial commit

---
+++ dist/_/inc/head.php
@@ -0,0 +1,16 @@
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width">
+        <!-- Custom Favicon -->
+        <link rel="shortcut icon" href="http://<?php echo($ServerName); ?>/favicon.ico">
+
+
+        <link rel="stylesheet" href="/_/css/bootstrap.css"/>
+
+        <link rel="stylesheet" href="/_/css/site-styles.css"/>
+
+        <script src="/_/js/lib/modernizr/modernizr.js"></script>
+
+<!--[if lt IE 9]>
+<script src="/_/js/respond.js"></script>
+<![endif]-->
---
+++ dist/_/inc/tail.php
@@ -0,0 +1,5 @@
+
+
+        <script src="/_/js/main.js"></script>
+
+        <script src="/_/js/bootstrap.js"></script>
