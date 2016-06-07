466d4bae82d4e93cf71b2b530d958d4fbf41681b
mandarln <mandarin@protonmail.com>
2016-06-07 03:54:26 +0300

    Create ca.php

---
+++ app/ca.php
@@ -0,0 +1,208 @@
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/_/inc/init.php'); ?>
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js ie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js ie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js ie ie8 lt-ie9"> <![endif]-->
+<!--[if IE 9]>
+<html class="no-js ie ie9 lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <title>Coin Army</title>
+    <meta name="description" content="Coin Army Telegram Group">
+    <?php require("_/inc/head.php"); ?>
+</head>
+<body>
+<?php require('_/inc/header.php'); ?>
+<div class="container" id="PageBody">
+    <div class="row row-centered">
+        <div class="col-xs-12 col-centered">
+            <h1>Coin Army</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-0 categoryContainer" data-id="2">
+            <h3 id="gettingstarted">Start</h3>
+            <div class="well">
+                <table class="table table-striped table-hover sortable-theme-bootstrap table-condensed" data-sortable="" data-sortable-initialized="true">
+                    <thead>
+                    <tr>
+                        <th class="text-left" width="20%">Division</th>
+                        <th class="text-left" width="50%">Objectiv</th>
+                        <th class="text-left" width="10%">Users</th>
+                        <th class="text-left" width="10%">General</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/BitcoinChat" target="_blank">Bitcoin Chat</a></td>
+                        <td class="text-left">All Bitcoin, Cryptocurrency related chat</td>
+                        <td class="text-left">51</td>
+                        <td class="text-left"></td>
+                        <td class="text-left"><a href="https://telegram.me/PilotTris" target="_blank">@PilotTris</a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-0 categoryContainer" data-id="2">
+            <h3 id="gettingstarted">Trading</h3>
+            <div class="well">
+                <table class="table table-striped table-hover sortable-theme-bootstrap table-condensed" data-sortable="" data-sortable-initialized="true">
+                    <thead>
+                    <tr>
+                        <th class="text-left" width="10%">Division</th>
+                        <th class="text-left" width="55%">Objectiv</th>
+                        <th class="text-left" width="10%">General</th>
+                        <th class="text-left" width="5%">Cadets</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/whalepoolbtc" target="_blank">Whalepool</a></td>
+                        <td class="text-left">Bitcoin, Crypto and legacy market traders</td>
+                        <td class="text-left"><a href="https://telegram.me/warlord1" target="_blank">@flibbr</a></td>
+                        <td class="text-left">651</td>
+                    </tr>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/joinchat/BpTIDjwx_ayUl5zN84uQBw" target="_blank">Whaleclub</a></td>
+                        <td class="text-left">Talk to other traders anytime of the day. Bitcoin markets never sleep!</td>
+                        <td class="text-left"><a href="https://telegram.me/btcvix" target="_blank">@BTCVIX</a></td>
+                        <td class="text-left">1324</td>
+                    </tr>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/thecoinfarm" target="_blank">The Coin Farm</a></td>
+                        <td class="text-left">Cryptocurrency TA traders</td>
+                        <td class="text-left">-</td>
+                        <td class="text-left">631</td>
+
+                    </tr>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/shitcoincharts" target="_blank">Shitcoin Charts</a></td>
+                        <td class="text-left">Shitcoin information - Triangles and pretty lines.</td>
+                        <td class="text-left"><a href="https://telegram.me/flibbr" target="_blank">@flibbr</a></td>
+                        <td class="text-left">46</td>
+                    </tr>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/shitcointalkromania" target="_blank">ShitcoinTalk</a></td>
+                        <td class="text-left">Shitcoin Army, Trading Bots</td>
+                        <td class="text-left"><a href="https://telegram.me/bombaucigasa" target="_blank">@Bomba...</a></td>
+                        <td class="text-left">68</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-0 categoryContainer" data-id="2">
+            <h3 id="gettingstarted">Local</h3>
+            <div class="well">
+                <table class="table table-striped table-hover sortable-theme-bootstrap table-condensed" data-sortable="" data-sortable-initialized="true">
+                    <thead>
+                    <tr>
+                        <th class="text-left" width="30%">Division</th>
+                        <th class="text-left" width="50%">Objectiv</th>
+                        <th class="text-left" width="10%">Users</th>
+                        <th class="text-left" width="10%">General</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/Bitcoinuk" target="_blank">Bitcoin UK</a></td>
+                        <td class="text-left">Bitcoin UK</td>
+                        <td class="text-left">33</td>
+                        <td class="text-left"><a href="https://telegram.me/rawavocado" target="_blank">@rawavocado</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">Deutsches Bitcoin</td>
+                        <td class="text-left">Alles über Bitcoin und Kryptowährung</td>
+                        <td class="text-left">48</td>
+                        <td class="text-left"><a href="https://telegram.me/whitesideofwhite" target="_blank">@whitesideofwhite</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left"><a href="httpshttps://telegram.me/joinchat/C9lLdT9Kwl7VQ81sLponXA" target="_blank">Bitcoin Kenya</a></td>
+                        <td class="text-left">Bitcoin Kenya</td>
+                        <td class="text-left">20</td>
+                        <td class="text-left"><a href="https://telegram.me/Cru23" target="_blank">@Cru23</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">Bitcoin in Romania</td>
+                        <td class="text-left">Bitcoin in Romania</td>
+                        <td class="text-left">50</td>
+                        <td class="text-left"><a href="https://telegram.me/warlord1" target="_blank">@warlord1</a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-0 categoryContainer" data-id="2">
+            <h3 id="gettingstarted">Advanced</h3>
+            <div class="well">
+                <table class="table table-striped table-hover sortable-theme-bootstrap table-condensed" data-sortable="" data-sortable-initialized="true">
+                    <thead>
+                    <tr>
+                        <th class="text-left" width="30%">Division</th>
+                        <th class="text-left" width="50%">Objectiv</th>
+                        <th class="text-left" width="10%">Users</th>
+                        <th class="text-left" width="10%">General</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="text-left"><a href="https://telegram.me/joinchat/AsVHrzvLq0SNnLuVGzZt6g" target="_blank">Coin Dojo 2.0</a></td>
+                        <td class="text-left">Bitcoin and Crypto Project Chat</td>
+                        <td class="text-left">286</td>
+                        <td class="text-left"><a href="https://telegram.me/ShawnLeary" target="_blank">@ShawnLeary</a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-0 categoryContainer" data-id="2">
+            <h3 id="gettingstarted">Businesses</h3>
+            <div class="well">
+                <table class="table table-striped table-hover sortable-theme-bootstrap table-condensed" data-sortable="" data-sortable-initialized="true">
+                    <thead>
+                    <tr>
+                        <th class="text-left" width="30%">Division</th>
+                        <th class="text-left" width="50%">Objectiv</th>
+                        <th class="text-left" width="10%">Users</th>
+                        <th class="text-left" width="10%">General</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="text-left">StartHash</td>
+                        <td class="text-left">Bitcoin mining farm</td>
+                        <td class="text-left">-</td>
+                        <td class="text-left"><a href="https://telegram.me/BombaUcigasa" target="_blank">@BombaUcigasa</a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--[if lt IE 9]>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser.
+    Please <a href="http://browsehappy.com/">upgrade your browser</a>
+    or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your
+    experience.</p>
+<![endif]-->
+
+<?php require('_/inc/footer.php'); ?>
+
+<!-- JAVASCRIPT -->
+<?php require('_/inc/analytics.php'); ?>
+
+<?php require('_/inc/tail.php'); ?>
+</body>
+</html>
